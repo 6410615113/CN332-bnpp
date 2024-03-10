b309dc2f89c7c0129a3ef8cddb564217aba29a7f
leepitchakan <88651584+6410615113@users.noreply.github.com>
2024-03-10 14:03:41 +0700

    Delete trafficmonitoring directory

--- trafficmonitoring/task/test.py
+++
@@ -1,8 +0,0 @@
-
-
-report_result = '../media/uploads/admin/98/object_tracking/loop.txt'
-report_car = []
-f = open(report_result, "r")
-for x in f:
-    report_car.append(x.split(','))
-print(report_car)
--- trafficmonitoring/user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Account
-# Register your models here.
-
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ("user", "img_profile", "role")
-
-admin.site.register(Account, AccountAdmin)
