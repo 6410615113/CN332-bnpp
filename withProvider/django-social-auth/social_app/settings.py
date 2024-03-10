f80f9e14796ab27aefaf18692bee36307f92cbb7
6410685017 <88651082+6410685017@users.noreply.github.com>
2024-03-10 21:23:26 +0700

    Update settings.py

--- withProvider/django-social-auth/social_app/settings.py
+++ withProvider/django-social-auth/social_app/settings.py
@@ -64,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware', #new
 ]
 
 ROOT_URLCONF = 'social_app.urls'
