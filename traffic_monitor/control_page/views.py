520bb8c2cf1c7783d1867797351aada4714c73ad
party <6410685215@student.tu.ac.th>
2024-04-05 02:35:04 +0700

    Add login_required decorator to profile and dashboard views

--- traffic_monitor/control_page/views.py
+++ traffic_monitor/control_page/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def login(request):
     return redirect('account_login')
 
+@login_required
 def profile(request):
     return profileRender(request, {})
 
 def profileRender(request, context):
     return render(request, 'account/profile.html', context)
+
+@login_required
+def dashboard(request):
+    return dashboardRender(request, {})
+
+def dashboardRender(request, context):
+    return render(request, 'dashboard/dashboard.html', context)
+
